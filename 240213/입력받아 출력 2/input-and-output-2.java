2059616f6438a546c0c300d1c066d02db8fe245b
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-13 23:24:10 +0900

    Title: 입력받아 출력 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a = sc.next();
+        String b = sc.next();
+        System.out.print(a + b);
+    }
+}
