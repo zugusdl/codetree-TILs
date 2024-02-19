b2179175142ecd4401d8e78213441deab0e0bb1d
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-19 21:07:13 +0900

    Title: 비교에 따른 연산, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240219/비교에 따른 연산/operation-based-on-comparison.java
+++ 240219/비교에 따른 연산/operation-based-on-comparison.java
@@ -9,7 +9,7 @@ public class Main {
         if(a > b) {
             System.out.print(a * b);
         } else {
-            System.out.print(a / b);
+            System.out.print(b / a);
         }
     }
 }
