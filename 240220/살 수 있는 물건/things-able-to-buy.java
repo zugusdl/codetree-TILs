b057c42861b8067c637789f44ab070f9e0dce224
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-20 15:27:31 +0900

    Title: 살 수 있는 물건, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240220/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int money = sc.nextInt();
+        if(money >= 3000) {
+            System.out.print("book");
+        } else if(money >= 1000) {
+            System.out.print("mask");
+        } else {
+            System.out.print("no");
+        }
+    }
+}
