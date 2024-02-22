f437972dfb1c44270e6705ef8f6c815deba0ee1d
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-22 19:02:45 +0900

    Title: 둘 중 하나의 배수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if((a % 3 == 0) || (a % 5 == 0)) {
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
