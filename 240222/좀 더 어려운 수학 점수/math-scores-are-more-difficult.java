bf813a9a8493fd7ccb363fe98730e4a939ddd917
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-22 19:23:43 +0900

    Title: 좀 더 어려운 수학 점수, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240222/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
+
+        if(aMath > bMath) {
+            System.out.print("A");
+        } else if(aMath == bMath) {
+            if(aEng > bEng) {
+                System.out.print("A");
+            }
+        } else {
+            System.out.print("B");
+        }
+    }
+}
