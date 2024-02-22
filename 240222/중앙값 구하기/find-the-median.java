ac7207f77b160b6cef3a34c226e977ced9e4a431
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-22 19:50:43 +0900

    Title: 중앙값 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/중앙값 구하기/find-the-median.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for(int i=0; i<3; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.print(arr[1]);
+    }
+}
