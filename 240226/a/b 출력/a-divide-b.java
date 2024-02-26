885efb0ceaa55a2e7fa2d4c5448d4c058cb0a978
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-02-26 20:37:26 +0900

    Title: a/b 출력, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240226/a/b 출력/a-divide-b.java
+++ 240226/a/b 출력/a-divide-b.java
@@ -7,8 +7,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int result = a/b;
-        System.out.print(result+".");
+        System.out.print((a / b) + ".");
 
         a %= b;
 
