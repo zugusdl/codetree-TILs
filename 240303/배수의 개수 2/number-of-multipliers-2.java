381db005c3edbb470ddea2929a248edbbfa2c23c
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-03 21:05:31 +0900

    Title: 배수의 개수 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240303/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        for(int i = 0; i < 10; i++) {
+            if(sc.nextInt() % 2 == 1) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
