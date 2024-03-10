cd46aabbbbee37fd48e01df492d204581eec53f5
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-10 21:41:35 +0900

    Title: 완전수 판별기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i < n; i++) {
+            if(n % i == 0) sum += i;
+        }
+
+        System.out.print(sum == n ? "P" : "N");
+    }
+}
