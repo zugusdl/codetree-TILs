4f9e94c3c741d12e3c95a094729366afd93e6a60
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-12 21:27:59 +0900

    Title: a의 배수들의 곱, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/a의 배수들의 곱/product-of-multiples-of-a.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int prod = 1;
+
+        for(int i = 1; i <= b; i++) {
+            if(i % a == 0) prod *= i;
+        }
+
+        System.out.print(prod);
+    }
+}
