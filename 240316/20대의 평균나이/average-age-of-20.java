7a2de7f3c7ea2fb759b89568fa9dda290925e425
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-16 22:59:12 +0900

    Title: 20대의 평균나이, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int cnt = 0;
+
+        while(true) {
+            int n = sc.nextInt();
+            if(n >= 30 || n <= 10) {
+                break;
+            }
+            sum += n;
+            cnt++;
+        }
+
+        System.out.printf("%.2f", (double)sum / cnt);
+    }
+}
