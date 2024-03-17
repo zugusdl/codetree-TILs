a2cf078ae9dbf77cdb74b48887a446b4bd59ff31
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-17 14:39:26 +0900

    Title: 조건에 따른 연산, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/조건에 따른 연산/operatino-by-rule.java
+++ 240317/조건에 따른 연산/operatino-by-rule.java
@@ -7,15 +7,16 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
         while(true) {
+            if(n >= 1000) {
+                System.out.print(cnt);
+                break;
+            }
+            
             if(n % 2 == 0)
                 n = (n * 3) + 1;
             else
                 n = (n * 2) + 2;
             cnt++;
-            if(n >= 1000) {
-                System.out.print(cnt);
-                break;
-            }
         }
     }
 }
