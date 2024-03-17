1b09ec65fe405c39c16e8c387d2ba7d92379fe70
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-17 14:06:42 +0900

    Title: 3n + 1 수열, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240317/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true) {
+            if(n == 1) {
+                System.out.print(cnt);
+                break;
+            }
+            if(n % 2 == 0) n /= 2;
+            else n = (n * 3) + 1;
+            cnt++;
+        }
+    }
+}
