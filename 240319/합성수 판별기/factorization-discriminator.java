1c5bb25e4e1156483f26447c651567937a1db70a
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-03-19 21:48:39 +0900

    Title: 합성수 판별기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240319/합성수 판별기/factorization-discriminator.java
+++ 240319/합성수 판별기/factorization-discriminator.java
@@ -5,15 +5,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int cnt = 0;
         boolean satiesfied = false;
 
-        for(int i = 1; i <= n; i++) {
+        for(int i = 2; i <= n; i++) {
             if(n % i == 0) {
-                cnt++;
-            }
-
-            if(cnt > 2) {
                 satiesfied = true;
                 break;
             }
