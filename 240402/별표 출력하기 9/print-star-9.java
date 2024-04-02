0fbdc92789b2e59a5bf872a4f4f344487450a406
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-02 23:48:10 +0900

    Title: 별표 출력하기 9, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int space = n - i - 1;
+            int star = 2 * i + 1;
+            for(int j = 0; j < space; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 0; j < star; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
