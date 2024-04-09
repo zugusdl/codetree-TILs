2baca5b1361826a7f855d79989cc582a36b021f9
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-09 19:10:29 +0900

    Title: 별 그리기, Time: 247ms, Memory: 14MB, Status: Passed - CodeTree

--- 240409/별 그리기/star-drawing.java
+++ 240409/별 그리기/star-drawing.java
@@ -6,25 +6,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++) {
-            int space = n - (i + 1);
-            int star = (2 * i) + 1;
-
-            for(int j = 0; j < space; j++) {
+            for(int j = 0; j < n - (i + 1); j++) {
                 System.out.print(" ");
             }
-            for(int j = 0; j < star; j++) {
+            for(int j = 0; j < (2 * i) + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
         for(int i = 0; i < n - 1; i++) {
-            int space = i + 1;
-            int star = (2 * n) - (2 * i) - 3;
-            
-            for(int j = 0; j < space; j++) {
+            for(int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
-            for(int j = 0; j < star; j++) {
+            for(int j = 0; j < (2 * n) - (2 * i) - 3; j++) {
                 System.out.print("*");
             }
             System.out.println();
