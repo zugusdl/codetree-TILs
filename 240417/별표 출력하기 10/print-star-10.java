02e1348682855557ac4e6c690aaafbcfee849d74
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-17 19:24:24 +0900

    Title: 별표 출력하기 10, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240417/별표 출력하기 10/print-star-10.java
+++ 240417/별표 출력하기 10/print-star-10.java
@@ -5,16 +5,13 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i < n * 2; i++) {
-            if(i % 2 == 0) {
-                for(int j = 0; j < 1 + (i / 2); j++) {
-                    System.out.print("* ");
-                }
-            } else {
-                for(int j = 0; j < n - (i - 1) / 2; j++) {
-                    System.out.print("* ");
-                }
-            }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+            
+            for(int j = 0; j < n - i; j++)
+                System.out.print("* ");
             System.out.println();
         }
 
