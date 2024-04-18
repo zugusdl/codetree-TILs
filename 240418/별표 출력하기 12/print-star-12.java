e1d11a6082a79f9765f4fb0094d65522b869fc69
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-18 21:14:45 +0900

    Title: 별표 출력하기 12, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240418/별표 출력하기 12/print-star-12.java
+++ 240418/별표 출력하기 12/print-star-12.java
@@ -9,7 +9,7 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(j % 2 == 0) {
-                    if(i % 2 == 0) {
+                    if(i == 0) {
                         System.out.print("* ");
                     } else {
                         System.out.print("  ");
