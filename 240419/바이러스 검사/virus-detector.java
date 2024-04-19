e8dd94ad67340c16e98139ea1895d436e5742c28
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-19 21:15:51 +0900

    Title: 바이러스 검사, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240419/바이러스 검사/virus-detector.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long answer = 0;
+        int n = sc.nextInt(); // 식당 수
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            int customer = sc.nextInt(); // 고객 수
+            arr[i] = customer;
+        }
+
+        int leader = sc.nextInt(); // 팀장 고객 수
+        int member = sc.nextInt(); // 팀원 고객 수
+        
+        for(int i = 0; i < arr.length; i++) {
+            answer += 1; // 팀장 더하기
+            arr[i] -= leader; // 팀장 고객 수 빼기
+
+            double m = Math.ceil((double)arr[i] / member);
+            answer += m;
+        }
+
+        System.out.print(answer);
+
+    }
+}
