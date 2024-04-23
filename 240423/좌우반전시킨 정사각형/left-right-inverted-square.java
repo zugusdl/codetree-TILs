4ea6d97f98b8f005dfdf89b870a576acbec77c2f
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-23 22:10:57 +0900

    Title: 좌우반전시킨 정사각형, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240423/좌우반전시킨 정사각형/left-right-inverted-square.java
+++ 240423/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -8,7 +8,7 @@ public class Main {
 
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j < n; j++) {
-                System.out.print((n * i) - (i * j) + " ");
+                System.out.print(i * (n - j) + " ");
             }
             System.out.println();
         }
