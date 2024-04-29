133eace203f61c97923405fa5f4d22f5ab47fc67
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-04-29 19:30:00 +0900

    Title: 좌우로 반복하며 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240429/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 240429/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -7,13 +7,12 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) {
-                for(int j = 1; j <= n; j++) {
-                    System.out.print(j);
+            for(int j = 0; j < n; j++) {
+                if(i % 2 == 0) {
+                    System.out.print(j + 1);
                 }
-            } else {
-                for(int j = n; j > 0; j--) {
-                    System.out.print(j);
+                else {
+                    System.out.print(n - j);
                 }
             }
             System.out.println();
