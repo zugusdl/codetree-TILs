0210ab97c4296e7ae9f85ed92d95579bfebbaa06
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-06-25 15:48:18 +0900

    Title: 특정 숫자 도달하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240625/특정 숫자 도달하기/reaching-specific-number.java
+++ 240625/특정 숫자 도달하기/reaching-specific-number.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
         int sum = 0;
         int avg = 0;
 
-        for (int i = 0; i < 10; i++) {
-            int num = sc.nextInt();
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-            if(num >= 250) {
+        for (int i = 0; i < 10; i++) {
+            if(arr[i] >= 250) {
                 break;
             }
-            sum += num;
+            sum += arr[i];
             avg++;
         }
 
