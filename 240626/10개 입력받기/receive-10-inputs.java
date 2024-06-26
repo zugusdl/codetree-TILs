f5e62f92c086a578772da691ea64ae9679767d46
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-06-26 15:10:01 +0900

    Title: 10개 입력받기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240626/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int cnt = 0;
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                break;
+            }
+            cnt++;
+        }
+
+        for(int i = 0; i < cnt; i++) {
+            sum += arr[i];
+        }
+
+        System.out.printf("%d %.1f", sum, (double) sum / cnt);
+    }
+}
