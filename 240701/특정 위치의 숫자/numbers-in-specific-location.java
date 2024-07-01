5d846e0bc691c7929dae49af5296b9267c4e44f8
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-07-01 18:38:35 +0900

    Title: 특정 위치의 숫자, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240701/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sum = arr[2] + arr[4] + arr[9];
+
+        System.out.print(sum);
+    }
+}
