d04dfe6bc07e05ea82803907cb0906663881d794
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-07-05 13:53:59 +0900

    Title: 일의 자리 배열, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240705/일의 자리 배열/array-with-units.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        
+        for(int i = 0; i < 10; i++) {
+            if(i < 2) {
+                arr[i] = sc.nextInt();
+            } else {
+                arr[i] = arr[i - 1] + arr[i - 2];
+                if(arr[i] >= 10) {
+                    arr[i] -= 10;
+                }
+            }
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
